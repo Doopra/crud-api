286ad7da5f0936acdb212896a3662ec9bd954bf3
doopra <mkpongsaviour7@gmail.com>
2023-09-13 08:40:58 +0100

    updating the test script

--- test_script.php
+++ test_script.php
@@ -45,3 +45,45 @@ $response = curl_exec($curl);
 echo $response;
 
 
+//  Modify the details of an existing person.
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => 'https://teerka.up.railway.app/api/3',
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'PUT',
+  CURLOPT_POSTFIELDS =>'{
+    "name": "mmudara gab"
+}',
+  CURLOPT_HTTPHEADER => array(
+    'Content-Type: application/json'
+  ),
+));
+
+$response = curl_exec($curl);
+
+echo $response;
+
+
+//  Remove a person by name
+
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => 'https://teerka.up.railway.app/api/3',
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'DELETE',
+));
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+echo $response;
